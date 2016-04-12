515ed961c09bd9fa9f9e74ffd51501b0519639c7
vtt01uk <vtt01uk@gmail.com>
2016-04-12 12:16:46 +0100

    Defined new and create actions. Created form partial to be rendered in index page

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -2,4 +2,18 @@ class PlaysController < ApplicationController
   def index
   end
 
+  def new
+    @play = Play.new
+  end
+  
+  def create
+    @play = Play.new(play_params)
+  end
+  
+  
+  private #private method exclusive to this controller
+  
+  def play_params
+      params.require(:play).permit(:title, :description, :director)
+  end
 end
